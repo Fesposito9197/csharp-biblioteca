7d8f3e1930f5ed4fdf5569077802ab3d44f55589
Fesposito9197 <kekkoesp8@gmail.com>
2023-03-21 13:48:55 +0100

    aggiunge classi utenti e documenti

---
+++ Biblioteca.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+public class Biblioteca
+{
+}
+
+public class Utenti
+{
+    string cognome;
+    string nome;
+    string email;
+    string password;
+    int recapitoTelefonico;
+}
+
+public class Documenti
+{
+    string codiceIdentificativo;
+    string titolo;
+    int anno;
+    string settore;
+    string scaffale;
+    string autore;
+}
+
