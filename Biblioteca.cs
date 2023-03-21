ad240b5c134a5d0184877e417ba580619736bf3c
Fesposito9197 <kekkoesp8@gmail.com>
2023-03-21 13:59:00 +0100

    aggiunge classi dvd e libri e costruttori

--- Biblioteca.cs
+++ Biblioteca.cs
@@ -17,6 +17,15 @@ public class Utenti
     string email;
     string password;
     int recapitoTelefonico;
+
+    public Utenti(string cognome, string nome, string email, string password, int recapitoTelefonico)
+    {
+        this.cognome = cognome;
+        this.nome = nome;
+        this.email = email;
+        this.password = password;
+        this.recapitoTelefonico = recapitoTelefonico;
+    }
 }
 
 public class Documenti
@@ -27,5 +36,35 @@ public class Documenti
     string settore;
     string scaffale;
     string autore;
+
+    public Documenti(string codiceIdentificativo, string titolo, int anno, string settore, string scaffale, string autore)
+    {
+        this.codiceIdentificativo = codiceIdentificativo;
+        this.titolo = titolo;
+        this.anno = anno;
+        this.settore = settore;
+        this.scaffale = scaffale;
+        this.autore = autore;
+    }
+}
+
+public class Libri : Documenti
+{
+    int numeroPagine;
+
+    public Libri(string codiceIdentificativo, string titolo, int anno, string settore, string scaffale, string autore, int numeroPagine) : base(codiceIdentificativo, titolo, anno, settore, scaffale, autore)
+    {
+        this.numeroPagine = numeroPagine;
+    }
+}
+
+public class Dvd : Documenti
+{
+    int durata;
+
+    public Dvd(string codiceIdentificativo, string titolo, int anno, string settore, string scaffale, string autore, int durata) : base(codiceIdentificativo, titolo, anno, settore, scaffale, autore)
+    {
+        this.durata = durata;
+    }
 }
 
